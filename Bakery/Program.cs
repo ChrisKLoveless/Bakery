566fb952f5a0bbffbbdd260f5eb841addb73f8ee
Chris Loveless <chriskloveless@gmail.com>
2022-12-09 10:00:54 -0800

    return user input and convert to int in Program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,7 +7,25 @@ namespace Bakery.Program
   {
     static void Main()
     {
-      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("\n Welcome to Pierre's Bakery!");
+      Console.WriteLine("Menu: \n");
+      Console.WriteLine("Loaf of Bread.....$5");
+      Console.WriteLine("[Buy 2 get 1 Free!] \n");
+      Console.WriteLine("A Yummy Pastry.....$2");
+      Console.WriteLine("[Every 3rd Pastry if Half Price!] \n");
+      Console.WriteLine("Would you like to place an Order? [Y/y for YES; Press Enter for NO]");
+
+      string orderResponse = Console.ReadLine().ToUpper();
+      if (orderResponse == "Y")
+      {
+        Console.WriteLine("\n How many Loaves of Bread? : ");
+        int loafCount = int.Parse(Console.ReadLine());
+        Console.WriteLine("\n How many Yummy Pastries? : ");
+        int pastryCount = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Bread Count: {loafCount}  Pastry Count: {pastryCount}");
+
+        
+      }
     }
   }
 }
